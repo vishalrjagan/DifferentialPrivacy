5ce2cabdbd3ffb017ef699883a0601d8a0a90c6a
Paul Krogmeier <paulmk2@illinois.edu>
2020-07-10 03:49:28 -0500

    pushing more data

--- AccuracyBash/svt_better_bounds.sh
+++ AccuracyBash/svt_better_bounds.sh
@@ -2,8 +2,14 @@ make acc_install_silent
 
 # svt1
 # make acc_silent INPUT=input_3_svt1.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 BOUNDFACTOR=1/7
-make acc_silent INPUT=input_3_svt1_c2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 C=2 BOUNDFACTOR=1/13
+# make acc_silent INPUT=input_3_svt1_c2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 C=2 BOUNDFACTOR=1/13
 
 # svt2
+# make acc_silent INPUT=input_3_svt2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 BOUNDFACTOR=1/7
+# make acc_silent INPUT=input_3_svt2_c2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 C=2 BOUNDFACTOR=1/13
+
+# Getting eps and failing inputs
+# make acc_silent INPUT=input_3_svt1.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 BOUNDFACTOR=1/7
+# make acc_silent INPUT=input_3_svt1_c2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 BOUNDFACTOR=1/14
 # make acc_silent INPUT=input_3_svt2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 BOUNDFACTOR=1/7
 make acc_silent INPUT=input_3_svt2_c2.txt TYPE=0 NUMQ=3 RANGEWIDTH=1 C=2 BOUNDFACTOR=1/13
